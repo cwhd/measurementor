e5f44b48edf744a8991250d589dfc71f269b9d88
cdav18 <chris.davis@nike.com>
2015-03-25 14:37:45 -0400

    test coverage!

--- test/unit/org/cwhd/measure/UtilitiesServiceSpec.groovy
+++ test/unit/org/cwhd/measure/UtilitiesServiceSpec.groovy
@@ -19,9 +19,41 @@ class UtilitiesServiceSpec extends Specification {
     def cleanup() {
     }
 
+    void "Email Gets Clean"() {
+        def cleanEmail = utilitiesService.cleanEmail(dirtyEmail)
+        expect: cleanEmail == "chris_davis.com"
+        where:
+            dirtyEmail << ["chris@davis.com"]
+    }
+
+    void "Clean Email Returns Nothing If You Give It Nothing"() {
+        def cleanEmail = utilitiesService.cleanEmail(dirtyEmail)
+        expect: cleanEmail == null
+        where:
+            dirtyEmail << [null]
+    }
+
+    void "Make Username Token Friendly"() {
+        def tokenFriendlyUserName = utilitiesService.makeNonTokenFriendly(nonTokenFriendlyUserName)
+        expect: tokenFriendlyUserName == "chrisdavisauthor"
+        where: nonTokenFriendlyUserName << ["chris-davis(author)_etw_whq"]
+    }
+
+    def "Token Cleaner Returns Nothing If You Give It Nothing"(){
+        def tokenFriendlyUserName = utilitiesService.makeNonTokenFriendly(nonTokenFriendlyUserName)
+        expect: tokenFriendlyUserName == null
+        where: nonTokenFriendlyUserName << [null]
+    }
+
+//    void "JIRA Date Gets Clean"() {
+//        def cleanJiraDate = utilitiesService.cleanJiraDate(rawJiraDate)
+//        expect: cleanJiraDate == new Date()
+//        where rawJiraDate << "2014-06-17T18:00:00.000+0000"
+//    }
+
+
     void "Build Name From Jenkins Gets Clean"() {
         def cleanBuildName = utilitiesService.cleanFullBuildName(fullBuildName)
-        println "build name result: $cleanBuildName"
         expect: cleanBuildName == "Chris >> Builds"
         where:
             fullBuildName << ["Chris >> Builds #56", "Chris >> Builds #1"]
@@ -30,7 +62,6 @@ class UtilitiesServiceSpec extends Specification {
 
     void "Test URL Cleaner"(){
         def cleanURL = utilitiesService.getPathFromUrl(url)
-        println "AND THE URL IS: $cleanURL"
         expect: cleanURL == "/job/HEYOO/"
 
         where:
@@ -40,23 +71,30 @@ class UtilitiesServiceSpec extends Specification {
 
     void "Test Time Difference"() {
         def dateDiff =  utilitiesService.getDifferenceBetweenDatesInHours(firstDate, secondDate)
-        println "ACTUAL VALUE IS: $dateDiff"
         expect: dateDiff == 0.9166666667
         where:
             firstDate << [1422639305990]
             secondDate << [1422642656258]
     }
 
+    void "Timestamp String To Date Returns Nothing If You Give It Nothing"() {
+        def theDate = utilitiesService.convertTimestampFromString(timestampString)
+        expect: theDate == null
+        where:
+        timestampString << [null]
+    }
+
+    /*
     void "Test String Timestamp To Date"() {
         def theDate = utilitiesService.convertTimestampFromString(timestampString)
-        println theDate
         expect: theDate == new Date(1421356703000L)
         where:
             timestampString << ["1421356703000"]
     }
-/*
+    */
+
     void "Test time-estimate ratio for a fibonacci estimates"() {
-        def estimateHealth = utilitiesService.estimateHealth(estimate, actualTime, 13, 9, fibonacciSeries)
+        def estimateHealth = utilitiesService.estimateHealth(estimate, actualTime, 13, 9, fibonacciSeries).result
         expect: estimateHealth == 0
 
         where:
@@ -65,7 +103,7 @@ class UtilitiesServiceSpec extends Specification {
     }
 
     void "Test time-estimate ratio for power of two estimates"() {
-        def estimateHealth = utilitiesService.estimateHealth(estimate, actualTime, 13, 9, powerOfTwoSeries)
+        def estimateHealth = utilitiesService.estimateHealth(estimate, actualTime, 13, 9, powerOfTwoSeries).result
         expect: estimateHealth == 0
 
         where:
@@ -73,6 +111,7 @@ class UtilitiesServiceSpec extends Specification {
         actualTime <<   [1, 1, 2, 2, 3, 4, 4, 5, 6, 7, 8, 8, 9]
     }
 
+    /*
     void "Test CHD Calculation with perfect result"(){
         def chd = utilitiesService.calculateCHD(cloc, estimateHealth, recidivism, escapedDefects)
         expect: chd == 100
@@ -104,21 +143,4 @@ class UtilitiesServiceSpec extends Specification {
 
     }
 **/
-
 }
-/*
-
-/**
- 16 points		9 days		7-9 days		devTime:[7 TO 9] AND storyPoints:16
- 8 points		4.5 days		4-7 days		devTime:[0 TO 1] AND storyPoints:8
- 4 points		2.25 days		2-4 days		devTime:[2 TO 4] AND storyPoints:4
- 2 points		1.125 days	1-2 days		devTime:[1 TO 2] AND storyPoints:2
- 1 point		.56 days		less than 1 day	devTime:[0 TO 1] AND storyPoints:1
- Fibbonaci series estimation with 2 week sprints
- 13 points		9		6-9 days
- 8 points		5.53		4-6 days
- 5 points		3.46		3-4 days
- 3 points		2.07		2-3 days
- 2 points		1.38		1-2 days
- 1 point		.69		less than a day
- **/
