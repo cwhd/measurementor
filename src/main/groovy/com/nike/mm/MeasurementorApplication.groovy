35ff4330e48529443f15a7f43fff3057b3ead856
Nicolas Richard <nicolas.richards@nike.com>
2015-06-17 09:16:13 -0400

    Enabling async processing in application

--- src/main/groovy/com/nike/mm/MeasurementorApplication.groovy
+++ src/main/groovy/com/nike/mm/MeasurementorApplication.groovy
@@ -2,9 +2,11 @@ package com.nike.mm
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 
 @SpringBootApplication
+@EnableAsync
 class MeasurementorApplication {
 
     static void main(String[] args) {
