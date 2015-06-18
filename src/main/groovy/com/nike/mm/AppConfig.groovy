50613caabed1ce7b0d926b2bee7e607f8ee3ee1b
Nicolas Richard <nicolas.richards@nike.com>
2015-06-17 23:05:58 -0400

    Adding cron job support

--- src/main/groovy/com/nike/mm/AppConfig.groovy
+++ src/main/groovy/com/nike/mm/AppConfig.groovy
@@ -7,21 +7,41 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 
 @Configuration
 @EnableSpringDataWebSupport
 class AppConfig {
-	
-	@Value('${mm.encrypt.password}')
-	String encryptPassword;
-	 
-	 @Bean StrongTextEncryptor strongTextEncryptor() {
-		 StrongTextEncryptor textEncryptor = new StrongTextEncryptor();
-		 textEncryptor.setPassword(this.encryptPassword);
-		 return textEncryptor;
-	 }
-	 
-	 @Bean Mapper dozerBeanMapper() {
-		 return new DozerBeanMapper()
-	 }
+
+    @Value('${mm.encrypt.password}')
+    String encryptPassword;
+
+    @Bean
+    StrongTextEncryptor strongTextEncryptor() {
+        StrongTextEncryptor textEncryptor = new StrongTextEncryptor();
+        textEncryptor.setPassword(this.encryptPassword);
+        return textEncryptor;
+    }
+
+    @Bean
+    Mapper dozerBeanMapper() {
+        return new DozerBeanMapper()
+    }
+
+    @Bean
+    protected ThreadPoolTaskExecutor serviceTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix("service-task-executor-");
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        return executor;
+    }
+
+    @Bean
+    protected ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        return scheduler;
+    }
 }
