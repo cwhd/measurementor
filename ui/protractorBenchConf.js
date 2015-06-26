691a60655f9795a70944e09d5c2f915cfab98966
aautushk <aautushk@teksystems.com>
2015-06-26 16:32:28 -0400

    - simple e2e performanse benchmark has been added;

---
+++ ui/protractorBenchConf.js
@@ -0,0 +1,38 @@
+var httpServer = require('http-server');
+
+exports.config = {
+    directConnect: true,
+
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            //Important in order for benchpress to be able to force garbage collection, if desired
+            'args': ['--js-flags=--expose-gc'],
+            //Important for benchpress to get timeline data from the browser
+            'perfLoggingPrefs': {
+                'traceCategories': 'blink.console,disabled-by-default-devtools.timeline'
+            }
+        },
+        loggingPrefs: {
+            performance: 'ALL'
+        }
+    },
+
+    specs: ['**/benchmark.spec.js'],
+    framework: 'jasmine2',
+
+    beforeLaunch: function() {
+        // var server = httpServer.createServer({
+        //     showDir: false
+        // });
+        // server.listen('8081', 'localhost', function() {
+        //     var uri = 'http://localhost:8081';
+        //     console.log('Starting up http-server, serving ' + server.root + ' on: ' + uri);
+        // });
+    },
+
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 240000
+    },
+};
--- ui/protractorConf.js
+++ ui/protractorConf.js
@@ -1,4 +1,10 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['build/protractor/spec.js']
+    specs: ['build/protractor/spec.js'],
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'args': ['show-fps-counter=true']
+        }
+    },
 };
