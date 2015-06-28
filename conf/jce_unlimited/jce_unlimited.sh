26ebb6afda16c1604cdc96f1f669a2c9da6c87ef
reasonthearchitect <rhys.personal@gmail.com>
2015-06-27 20:58:39 -0400

    Added the scripts for updating JCE to unlimited.

--- conf/jce_unlimited/jce_unlimited.sh
+++ conf/jce_unlimited/jce_unlimited.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 sudo cp /usr/lib/jvm/java-8-oracle/jre/lib/security/local_policy.jar /usr/lib/jvm/java-8-oracle/jre/lib/security/local_policy.jar.bk
 
 sudo cp /usr/lib/jvm/java-8-oracle/jre/lib/security/US_export_policy.jar /usr/lib/jvm/java-8-oracle/jre/lib/security/US_export_policy.jar.bk
