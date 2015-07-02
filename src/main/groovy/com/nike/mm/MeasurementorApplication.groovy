ac93bf2d48bc383d66ea9dfbce0236131cd25466
Nicolas Richard <nicolas.richards@nike.com>
2015-07-02 13:40:55 -0400

    Changed configuration to use jetty (cont). Using correct class so that servlet deployment actually takes place

--- src/main/groovy/com/nike/mm/MeasurementorApplication.groovy
+++ src/main/groovy/com/nike/mm/MeasurementorApplication.groovy
@@ -2,15 +2,14 @@ package com.nike.mm
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration
+import org.springframework.boot.context.web.SpringBootServletInitializer
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
-
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-class MeasurementorApplication extends WebMvcAutoConfiguration {
+class MeasurementorApplication extends SpringBootServletInitializer {
 
     static void main(String[] args) {
         SpringApplication.run MeasurementorApplication, args
