92d7c35d609069d5619964f14d5095c89c2abac3
Nicolas Richard <nicolas.richards@nike.com>
2015-07-04 12:36:41 -0400

    JiraHistory

---
+++ src/main groovy/com/nike/mm/entity/JiraHistory.groovy
@@ -0,0 +1,19 @@
+package com.nike.mm.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.elasticsearch.annotations.Document
+
+
+@Document(indexName = "measurementor", type = "jirahistory")
+class JiraHistory {
+
+    @Id
+    String id
+    Date timestamp
+    String changedBy
+    String changeField
+    String newValue
+    String key
+    String sourceId
+    String dataType
+}
