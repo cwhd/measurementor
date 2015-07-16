e003d5ab3844c15a2d7a5455ff052803fa848e9d
Rhys Parry <rparry@AGLC02MQ5P9FD56.local>
2015-07-16 13:16:48 -0400

    Renamed the classes to PleaseWork

--- src/test/groovy/com/nike/mm/business/plugin/PleaseWorkJiraSpec.groovy
+++ src/test/groovy/com/nike/mm/business/plugin/PleaseWorkJiraSpec.groovy
@@ -1,38 +1,39 @@
 package com.nike.mm.business.plugin
 
 import com.nike.mm.MeasurementorApplication
-import com.nike.mm.business.plugins.IStashBusiness
+import com.nike.mm.business.plugins.IJiraBusiness
 import com.nike.mm.dto.HttpRequestDto
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
+import spock.lang.Specification
 
 import java.text.SimpleDateFormat
 
 /**
- * Created by rparry on 7/16/15.
+ * Created by rparry on 7/15/15.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MeasurementorApplication.class)
 @WebAppConfiguration
-class StashPleaseWorkSpec {
+class PleaseWorkJiraSpec extends Specification {
 
-    @Autowired IStashBusiness stashBusiness
+    @Autowired IJiraBusiness jiraBusiness
 
-    def "stash please work"() {
+    @Test
+    def "lets see if we can get some data"() {
 
         setup:
         //def config = [url:"", credentials: ""] as HttpRequestDto
         1 == 1
 
         when:
-        //this.stashBusiness.updateDataWithResponse(new SimpleDateFormat("dd/MM/yyyy").parse("01/07/2015"), config);
+        //this.jiraBusiness.updateDataWithResponse(new SimpleDateFormat("dd/MM/yyyy").parse("01/07/2015"), config);
         1 == 1
-
         then:
         1 == 1
     }
-
 }
--- src/test/groovy/com/nike/mm/business/plugin/PleaseWorkStashSpec.groovy
+++ src/test/groovy/com/nike/mm/business/plugin/PleaseWorkStashSpec.groovy
@@ -1,39 +1,38 @@
 package com.nike.mm.business.plugin
 
 import com.nike.mm.MeasurementorApplication
-import com.nike.mm.business.plugins.IJiraBusiness
+import com.nike.mm.business.plugins.IStashBusiness
 import com.nike.mm.dto.HttpRequestDto
-import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
-import spock.lang.Specification
 
 import java.text.SimpleDateFormat
 
 /**
- * Created by rparry on 7/15/15.
+ * Created by rparry on 7/16/15.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MeasurementorApplication.class)
 @WebAppConfiguration
-class JiraPleaseWorkSpec extends Specification {
+class PleaseWorkStashSpec {
 
-    @Autowired IJiraBusiness jiraBusiness
+    @Autowired IStashBusiness stashBusiness
 
-    @Test
-    def "lets see if we can get some data"() {
+    def "stash please work"() {
 
         setup:
         //def config = [url:"", credentials: ""] as HttpRequestDto
         1 == 1
 
         when:
-        //this.jiraBusiness.updateDataWithResponse(new SimpleDateFormat("dd/MM/yyyy").parse("01/07/2015"), config);
+        //this.stashBusiness.updateDataWithResponse(new SimpleDateFormat("dd/MM/yyyy").parse("01/07/2015"), config);
         1 == 1
+
         then:
         1 == 1
     }
+
 }
