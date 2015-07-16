03d8091b1a8a8c097a6ec6b84d0b739dedb129ee
aautushk <aautushk@teksystems.com>
2015-07-16 13:55:03 -0400

    - build version displayed in the header of the app;

--- ui/app/general-layout/partial/general-wrapper/general-wrapper.ts
+++ ui/app/general-layout/partial/general-wrapper/general-wrapper.ts
@@ -1,10 +1,16 @@
 declare
 var angular;
 
+declare
+var versionSettings;
+
 angular.module("generalLayout").controller("GeneralWrapperCtrl", function($scope, generalLayout) {
     $scope.onLogOut = function() {
         generalLayout.logOut();
     };
 
     $scope.generalLayoutData = generalLayout.data;
+    
+    $scope.version = versionSettings;
+    alert(versionSettings.revision);
 });
